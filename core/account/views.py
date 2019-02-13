5a6a83f7d2de09519a127580a87c3fef831eb998
Guinhu <you@example.com>
2019-02-13 12:43:56 -0200

    Updaded django

--- core/account/views.py
+++ core/account/views.py
@@ -7,11 +7,10 @@ from rest_framework.permissions import AllowAny
 from .serializers import UserProfileSerializer
 from .models import UserProfile
 
+
 class UserProfileAPIView(CreateAPIView):
     """ Handles creating profiles & Allow any user (authenticated or not) to access this endpoint"""
 
-    queryset            = UserProfile.objects.all()
-    serializer_class    = UserProfileSerializer
-    permission_classes  = (AllowAny, )
-
-
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = (AllowAny, )
